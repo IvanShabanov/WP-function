b6f4a396e3a304853009aa4c4c30ef6757a02e8b
Ivan Shabanov <Ivan.shabanov@gmail.com>
2020-03-13 10:23:07 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php get_header(); ?>
 <div class="content">
 <?php
+    $current_slug = trim($_SERVER['REQUEST_URI'], '/');
     if (isset( $GLOBALS['SHOWTHIS'])){
         echo apply_filters('the_content', $GLOBALS['SHOWTHIS']['post_content']);
-    } else if (strpos($current_slug, 'catalog') !== false) {
+    } else if (strpos($current_slug, IS_catalog_slug) !== false) {
         the_post();
         the_content();
-        echo showChildren( array('template'=>"catalog", 'post_type'=>'catalog', 'id'=>$post->ID ) );
+        echo showChildren( array('template'=>"catalog", 'post_type'=>IS_catalog_slug, 'id'=>$post->ID ) );
     } else {
         if (have_posts()) {
             the_post();
