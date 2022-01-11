7507c1f8f83484106327b85bb255e0b207b2565b
Иван Шабанов <coder10@web-axioma.ru>
2022-01-11 12:47:42 +0700

    Добавлена пагинация

--- function.php
+++ function.php
@@ -1,14 +1,14 @@
 <?
    /****** Показать потомков страницы ************
     * $attr=array(
-    *   'id' - id родителя 
-    *   'template' - шаблон файл в папке templates файл .php 
+    *   'id' - id родителя
+    *   'template' - шаблон файл в папке templates файл .php
     *                   в файле foreach для массива pages
     *   'numberposts' - кол-во постов/страниц, по умолчанию 999
     *   'orderby' - поле сортировки, по умолчанию menu_order
     *   'iscat' - если родитель это категория по ставим "Y", тогда найдет все записи в категории
     *   'post_type' - тип постов которые нам нужны, по умолчанию page
-    * 
+    *
     *   Использовать в страницах так
     *   [showChildren id=1 template="sidemenu"][/showChildren]
     */
@@ -21,14 +21,12 @@
                 $template = 'pages';
             };
             $filter = array(
-                        'post_status' => 'publish', 
+                        'post_status' => 'publish',
                     );
             if (is_numeric($attr['numberposts'])) {
                 $filter['numberposts'] = $attr['numberposts'];
-            } else {
-                $filter['numberposts'] = 999;
             };
-        
+
             if ($attr['orderby']!= '') {
                 $filter['orderby'] = $attr['orderby'];
             } else {
@@ -49,7 +47,21 @@
             } else {
                 $filter['post_type'] = 'page';
             };
-            
+
+            if (is_numeric($attr['posts_per_page'])) {
+                $total_items = count(get_posts($filter));
+                $max_pages = max(1, ceil($total_items / $attr['posts_per_page']));
+                $filter['posts_per_page'] = $attr['posts_per_page'];
+                if (is_front_page()) {
+                    $currentPage = (get_query_var('page')) ? get_query_var('page') : 1;
+                } else {
+                    $currentPage = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                }
+                if (is_numeric($currentPage)) {
+                    $filter['offset'] = ($currentPage -1) * $filter['posts_per_page'];
+                }
+            };
+
             $pages = get_posts($filter);
             ob_start();
             include('templates/'.$template.'.php');
@@ -61,4 +73,31 @@
 
     add_shortcode( 'showChildren' , 'showChildren' );
 
+
+    function show_pagination($max_pages = null)
+    {
+        global $wp_query;
+        if (!is_numeric($max_pages)) {
+            $max_pages =  $wp_query->max_num_pages;
+        }
+
+
+        if (is_front_page()) {
+            $currentPage = (get_query_var('page')) ? get_query_var('page') : 1;
+        } else {
+            $currentPage = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        }
+
+        $pagination = paginate_links([
+                                         'base'      => str_replace(999999999, '%#%', get_pagenum_link(999999999)),
+                                         'format'    => '',
+                                         'current'   => max(1, $currentPage),
+                                         'total'     => $max_pages,
+                                         'type'      => 'list',
+                                         'prev_text' => '«',
+                                         'next_text' => '»',
+                                     ]);
+
+        return str_replace('page-numbers', 'pagination', $pagination);
+    }
 ?>
--- templates/pages.php
+++ templates/pages.php
@@ -1,6 +1,8 @@
 <?php
 if (is_array($pages)) {
     $result = '';
+    $result .= '<section>';
+    $result .= '<div>';
     foreach ($pages as $page) {
         $link = get_permalink($page);
         $title = $page->post_title;
@@ -12,6 +14,11 @@ if (is_array($pages)) {
         $result .= '<div class="text">'.$text.'</div>';
         $result .= '</div>';
     };
+    $result .= '</div>';
+    if ($max_pages > 1) {
+        $result .= show_pagination($max_pages);
+    }
+    $result .= '</section>';
     echo $result;
 };
 ?>
